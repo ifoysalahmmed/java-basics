53b1fc77a666134e3b2becb3685918e114695139
Foysal Ahmmed Limon <ifoysalahmmed@gmail.com>
2024-04-26 20:55:22 +0600

    added JavaVariables

---
+++ JavaVariables/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Aman";
+        int age = 30;
+        String neighbour = "Maya";
+
+        String friend = neighbour;
+        System.out.println(friend);
+    }
+}
