f669e4f9958aabfb00682e803191b4aa08b1b87e
Foysal Ahmmed Limon <ifoysalahmmed@gmail.com>
2024-04-26 20:51:18 +0600

    added JavaComments

---
+++ JavaComments/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+//        my first program
+        System.out.println("Hello World"); // println() creates new line after executing the command
+        System.out.print("Hello World"); // print() doesn't create new line after executing the command
+    }
+}
