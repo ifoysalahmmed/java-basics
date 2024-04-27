219f9fc4dccd3ef25661d6c0c6ea77cce4595174
Foysal Ahmmed Limon <ifoysalahmmed@gmail.com>
2024-04-27 19:38:44 +0600

    added JavaMathClass

---
+++ JavaMathClass/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+//        5, 6
+//        System.out.println(Math.max(5, 6));
+//        System.out.println(Math.min(5, 6));
+
+//        random
+        System.out.println(Math.random());
+        System.out.println((int) (Math.random() * 10000));
+    }
+}
