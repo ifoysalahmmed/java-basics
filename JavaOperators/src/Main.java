7ec896e2b63424e0d8abff2e343aad9df8d59651
Foysal Ahmmed Limon <ifoysalahmmed@gmail.com>
2024-04-27 19:31:02 +0600

    added JavaOperators

---
+++ JavaOperators/src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 2;
+
+        int sum = a + b;
+        int diff = a - b;
+        int multiply = a * b;
+        int div = a / b;
+        int mod = a % b;
+
+//        System.out.println(sum);
+//        System.out.println(diff);
+//        System.out.println(multiply);
+//        System.out.println(div);
+//        System.out.println(mod);
+
+        int numb = 1;
+        numb = numb + 1;
+
+        System.out.println(numb);
+
+//        another way is
+        int numb2 = 1;
+        numb2++; // urinary operator
+        System.out.println(numb2);
+    }
+}
