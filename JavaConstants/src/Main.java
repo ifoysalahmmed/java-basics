eb3d4e312a018f7cbb2cb7748161ae557370c5e7
Foysal Ahmmed Limon <ifoysalahmmed@gmail.com>
2024-04-27 18:50:04 +0600

    added JavaConstants

---
+++ JavaConstants/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 30;
+        age = 31;
+        age = 32;
+
+        final float PI = 3.14F;
+        PI = 1.1F;
+    }
+}
