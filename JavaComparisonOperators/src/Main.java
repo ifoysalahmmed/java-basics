ba1b23aee7c9a66f6173f75c9a3cb5b48addde0f
Foysal Ahmmed Limon <ifoysalahmmed@gmail.com>
2024-05-02 16:29:02 +0600

    added JavaComparisonOperators

---
+++ JavaComparisonOperators/src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+    public static void main(String[] args) {
+//        comparison operators
+
+//        a == b
+//        a != b
+//        a > b
+//        a < b
+//        a >= b
+//        a <= b
+
+//        conditional Statements
+
+//        boolean isSunUp = true;
+        boolean isSunUp = false;
+
+        if (isSunUp == true)
+            System.out.println("day");
+        else
+            System.out.println("night");
+
+//        int age = 30;
+        int age = 3;
+
+        if (age > 18)
+            System.out.println("can vote");
+        else
+            System.out.println("can't vote");
+
+
+    }
+}
