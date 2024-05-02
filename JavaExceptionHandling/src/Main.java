0ce1d237577c31da471862964b402635309a7b72
Foysal Ahmmed Limon <ifoysalahmmed@gmail.com>
2024-05-02 21:18:31 +0600

    added JavaExceptionHandling

---
+++ JavaExceptionHandling/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+//        TRY - CATCH Exception Handling
+        int[] marks = {97, 98, 94};
+        try {
+            System.out.println(marks[5]);
+        } catch (Exception exception) {
+//            do something after catching
+//            System.out.println(exception);
+        }
+        System.out.println("The name is Aman");
+    }
+}
