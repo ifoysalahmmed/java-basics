405a6cb59b9a0dbc82487218d196d0b734b1130a
Foysal Ahmmed Limon <ifoysalahmmed@gmail.com>
2024-05-02 21:32:22 +0600

    added JavaFunctions

---
+++ JavaFunctions/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void printJava() {
+        System.out.println("Hello Java!");
+    }
+
+    public static void printName(String name) {
+        System.out.println(name);
+    }
+
+    public static void printSum(int a, int b) {
+        int sum = a + b;
+        System.out.println(sum);
+    }
+
+    public static void main(String[] args) {
+//        printJava();
+//        printJava();
+//        printJava();
+
+//        printName("Aman");
+//        printName("Akku");
+
+        printSum(5, 6);
+        printSum(1, 3);
+    }
+}
